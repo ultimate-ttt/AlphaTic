9f39e44676525348a5a8085d5d311d86b469fc22
maracuja-juice <16801528+maracuja-juice@users.noreply.github.com>
2019-04-14 13:53:54 +0200

    implement get_winresult

--- GameEngine.py
+++ GameEngine.py
@@ -18,8 +18,8 @@ class Player:
 class Winner:
     cross = 0
     circle = 1
-    draw = 2
-    none = 3
+    none = 2
+    draw = 3
 
     def winner_as_string(self, winner: int):
         if winner == self.cross:
@@ -32,7 +32,6 @@ class Winner:
             return '.'
 
 
-# TODO: Convert to real enums?
 class TileValue:
     cross = 0
     circle = 1
@@ -59,26 +58,50 @@ class Move:
         self.tile_position = tile_position
 
 
-class SmallTileInformation:
-    board_position: Point
+class TileInformation:
     position: Point
     value: int
 
-    def __init__(self, board_position: Point, position: Point, value: int):
-        self.board_position = board_position
+    def __init__(self, position: Point, value: int):
         self.position = position
         self.value = value
 
 
-class SmallBoardInformation:
-    position: Point
-    value: int
+class SmallTileInformation(TileInformation):
+    board_position: Point
+
+    def __init__(self, board_position: Point, position: Point, value: int):
+        TileInformation.__init__(self, position, value)
+        self.board_position = board_position
+
+
+class SmallBoardInformation(TileInformation):
     tiles: [SmallTileInformation]
 
     def __init__(self, position: Point, value: int, tiles: [SmallTileInformation]):
-        self.position = position
-        self.value = value
+        TileInformation.__init__(self, position, value)
         self.tiles = tiles
+
+
+class LineWinResult:
+    line: str
+    won: bool
+
+    def __init__(self, line: str, won: bool):
+        self.line = line
+        self.won = won
+
+
+class WinResult:
+    finished: bool
+    winner: int
+
+    def __init__(self, finished: bool, winner: int):
+        self.finished = finished
+        self.winner = winner
+
+    def __repr__(self):
+        return 'Finished: ' + str(self.finished) + '\nWinner: ' + Winner().winner_as_string(self.winner)
 
 
 class TicTacToeGame:
@@ -98,14 +121,19 @@ class TicTacToeGame:
             self.apply_move(move)
 
     def apply_move(self, move):
-        self.moves.append(move)
-
         board_to_change = next(board for board in self.board if board.position == move.board_position)
         tile_to_change = next(tile for tile in board_to_change.tiles if tile.position == move.tile_position)
         tile_to_change.value = self.current_player
 
-        # TODO calculate win result
-        # TODO change player
+        win_result = self.__get_winresult_for_board(board_to_change.tiles)
+        if win_result.finished:
+            board_to_change.value = win_result.winner
+
+        self.moves.append(move)
+        self.__change_player()
+
+    def get_winresult(self):
+        return self.__get_winresult_for_board(self.board)
 
     def print_game(self):
         smallboard_size = 3
@@ -151,3 +179,55 @@ class TicTacToeGame:
                 tile = SmallTileInformation(board_position, Point(x, y), TileValue.empty)
                 tiles.append(tile)
         return tiles
+
+    def __change_player(self):
+        if self.current_player == Player.cross:
+            self.current_player = Player.circle
+        else:
+            self.current_player = Player.cross
+
+    def __get_winresult_for_board(self, board: [TileInformation]):
+        cross_won = self.__get_win_result_for_player(Player.cross, board)
+        circle_won = self.__get_win_result_for_player(Player.circle, board)
+        draw = all(element.value != TileValue.empty for element in board)
+
+        if cross_won:
+            return WinResult(True, Winner.cross)
+        elif circle_won:
+            return WinResult(True, Winner.circle)
+        elif draw:
+            return WinResult(True, Winner.draw)
+        else:
+            return WinResult(False, Winner.none)
+
+    def __get_win_result_for_player(self, player: int, board: [TileInformation]):
+        row0 = filter(lambda el: el.position.x == 0, board)
+        row1 = filter(lambda el: el.position.x == 1, board)
+        row2 = filter(lambda el: el.position.x == 2, board)
+        column0 = filter(lambda el: el.position.y == 0, board)
+        column1 = filter(lambda el: el.position.y == 1, board)
+        column2 = filter(lambda el: el.position.y == 2, board)
+        leftslant = filter(lambda el: el.position.x == el.position.y, board)
+        rightslant = filter(lambda el: el.position.x + el.position.y == 2, board)
+
+        possible_win_positions = [
+            LineWinResult('row0', self.__has_won_line(player, row0)),
+            LineWinResult('row1', self.__has_won_line(player, row1)),
+            LineWinResult('row2', self.__has_won_line(player, row2)),
+            LineWinResult('column0', self.__has_won_line(player, column0)),
+            LineWinResult('column1', self.__has_won_line(player, column1)),
+            LineWinResult('column2', self.__has_won_line(player, column2)),
+            LineWinResult('leftSlant', self.__has_won_line(player, leftslant)),
+            LineWinResult('rightSlant', self.__has_won_line(player, rightslant)),
+        ]
+
+        for possible_win in possible_win_positions:
+            if possible_win.won:
+                return True
+        return False
+
+    def __has_won_line(self, player: int, row: [TileInformation]):
+        return self.__count_in_line(player, row) == 3
+
+    def __count_in_line(self, player: int, row: [TileInformation]):
+        return len(list(filter(lambda el: el.value == player, row)))
